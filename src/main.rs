9f7e968f638d3f0befb986ce8adb7738d881d366
joelobros23 <laberintoj8@gmail.com>
2025-06-13 09:15:36 +0800

    fix: Attempt to fix build error for goal '1. Set up the basic eframe application boilerplate in `src/main.rs` and create basic file structure'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::{egui, App, Frame, NativeOptions};
+
+#[derive(Default)]
+struct CodeFlowApp {
+    name: String,
+    age: u32,
+}
+
+impl App for CodeFlowApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
+}
+
+fn main() -> Result<(), eframe::Error> {
+    tracing_subscriber::fmt::init();
+
+    let options = NativeOptions {
+        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "CodeFlow",
+        options,
+        Box::new(|_cc| Box::new(CodeFlowApp::default())),
+    )
 }
